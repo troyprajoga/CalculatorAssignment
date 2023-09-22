a239de9b5771a0c89ed2637130d89355477d0591
troyprajoga <127496093+troyprajoga@users.noreply.github.com>
2023-09-22 21:41:01 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,9 @@
 while True:
     try:
-        inputOne = input("Please enter first number: ")
-        inputOne = float(inputOne)
+        weight,height = input("Please enter (weight in kilograms, height in centimeters): ").split(",")
+        weight = float(weight)
+        height = float(height)
         break
     except ValueError:
-        print("Invalid input. Please enter a valid float.")
-while True:
-    try:
-        inputTwo = input("Please enter second number: ")
-        inputTwo = float(inputTwo)
-        break
-    except ValueError:
-        print("Invalid input. Please enter a valid float.")
-while True:
-    inputOp = input("Please enter an arithmetic operation (+, -, *, /, **): ")
-    if inputOp in ['+', '-', '*', '/', '**']:
-        break
-    else:
-        print("Invalid input. Please enter a valid arithmetic operation.")
-
-if inputOp == '+':
-    output = float(inputOne + inputTwo)
-elif inputOp == '-':
-    output = float(inputOne - inputTwo)
-elif inputOp == '/':
-    output = float(inputOne / inputTwo)
-elif inputOp == '*':
-    output = float(inputOne * inputTwo)
-else:
-    output = float(inputOne ** inputTwo)
-print(str(inputOne) + str(inputOp) + str(inputTwo) + " = " + str(output))
+        print("Invalid input. Please enter valid floats.")
+print("Your BMI is: " + str(weight/(height/100)**2))
